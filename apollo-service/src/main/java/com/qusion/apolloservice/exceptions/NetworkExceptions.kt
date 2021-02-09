fb1b08c07a29ce1928980204a110948c9f00a497
Standa <stanislav@qusion.com>
2021-02-09 23:18:11 +0100

    makes exceptions open

--- apollo-service/src/main/java/com/qusion/apolloservice/exceptions/NetworkExceptions.kt
+++ apollo-service/src/main/java/com/qusion/apolloservice/exceptions/NetworkExceptions.kt
@@ -2,10 +2,10 @@ package com.qusion.apolloservice.exceptions
 
 /** Added as a cause when there are "business" errors from the response of the endpoint
  * (e.g. wrong password or username) */
-class BusinessException(override val message: String?) : Exception(message)
+open class BusinessException(override val message: String?) : Exception(message)
 
 /** Thrown by the http interceptor when there is 401 from the server */
 open class ExpiredSidException(override val message: String?) : Exception(message)
 
 /** Thrown by ApolloService when refresh token mutation fails */
-class ForceLogoutException(code: String) : ExpiredSidException(code)
+open class ForceLogoutException(code: String) : ExpiredSidException(code)
